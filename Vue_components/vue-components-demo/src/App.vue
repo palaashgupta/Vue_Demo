9e9fe41c4a8a219044a2d5b65ea74865cfccd8ae
palaashgupta <gpalaash@gmail.com>
2025-09-28 17:20:06 +0200

    Introducing Vue and Initiating Project

---
+++ Vue_components/vue-components-demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{message}} </h1>
+    </div>
+</template>
+
+<script>
+export default{
+        setup(){
+            const message="Hello Vue";
+            return{
+                message
+            };
+        },
+}
+</script>
+
+<style></style>
