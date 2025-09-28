ec04360211a7c0ebc96733f3494b4d23a7a95801
palaashgupta <gpalaash@gmail.com>
2025-09-28 19:26:08 +0200

    Performing some chnages with global and local components

--- Vue_components/vue-components-demo/src/App.vue
+++ Vue_components/vue-components-demo/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <h1>{{message}} </h1>
+    <div class="container">
+        <div>
+            <h1>{{message}}</h1>
+        </div>
+        
+        <Contact name="Palaash" :phone= "12312313" :ownername="contactsowner" email="gpalaash@gmail.com"></Contact>
+
+        <Contact name="Gupta" :phone= "7482373723" :ownername="contactsowner2" email="palaashgupta2584@gmail.com"></Contact>
+
     </div>
 </template>
 
-<script>
-export default{
-        setup(){
-            const message="Hello Vue";
-            return{
-                message
-            };
-        },
-}
+<script setup>
+import Contact from './components/Contact.vue';
+
+const message="Hello Vue";
+const contactsowner ="TEST";
+const contactsowner2 ="Exam";
 </script>
 
 <style></style>
---
+++ Vue_components/vue-components-demo/src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-info rounded p-4 m-2">
+        <h3>Name:{{ name }}</h3>
+        <p>Email: {{ email }}</p>
+        <p>Phone: {{ phone }}</p>
+        <p class="float-end small" v-if="ownername!=''">
+            **This contact info belongs to {{ ownername }}
+        </p>
+    </div>
+</template>
+<script setup>
+
+//const email =ref("palaashgupta2584@gmail.com");
+const props = defineProps({
+    name:{type:String,required:true},
+    phone:Number,
+    ownername:String,
+    email: {type:String,required:false, default:"Test@test.com"    }
+    });
+</script>
